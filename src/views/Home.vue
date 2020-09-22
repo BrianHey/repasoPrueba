d22b94b855c108655ef31bb4933c615174862c2d
Tamara <tamara.fendez@gmail.com>
2020-09-22 19:42:55 -0300

    crear formulario y link a inv

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,68 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    
+    <b-form @submit="onSubmit" @reset="onReset">
+      <b-form-group
+        id="input-group-1"
+        label="Email address:"
+        label-for="input-1"
+        description="We'll never share your email with anyone else."
+      >
+        <b-form-input
+          id="input-1"
+          
+          type="email"
+          required
+          placeholder="Enter email"
+        ></b-form-input>
+      </b-form-group>
+
+      <b-form-group id="input-group-2" label="Your Name:" label-for="input-2">
+        <b-form-input
+          id="input-2"
+          
+          required
+          placeholder="Enter name"
+        ></b-form-input>
+      </b-form-group>
+
+      <b-form-group id="input-group-3" label="otro input" label-for="input-2">
+        <b-form-input
+          id="input-2"
+          
+          required
+          placeholder="Enter name"
+        ></b-form-input>
+      </b-form-group>
+
+      <b-form-group id="input-group-2" label="cuarto input" label-for="input-2">
+        <b-form-input
+          id="input-2"
+          
+          required
+          placeholder="Enter name"
+        ></b-form-input>
+      </b-form-group>
+      
+
+      <b-button type="submit" variant="primary">Submit</b-button>
+      
+    </b-form>
+    <b-card class="mt-3" header="Form Data Result">
+      <pre class="m-0">{{ form }}</pre>
+    </b-card>
   </div>
 </template>
+  
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    
   }
 }
 </script>
---
+++ src/views/Inventario.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+      <h1>Inventario</h1>
+  </div>
+</template>
+
+<script>
+export default {
+name: 'Inventario'
+}
+</script>
+
+<style>
+
+</style>
